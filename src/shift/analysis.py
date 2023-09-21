022f0050e6fa852c31ed700ca4da96e1c226372e
Alejandra Jaramillo <aljaramillom@unal.edu.co>
2023-09-21 14:40:38 -0500

    anadir funciones cifrado y descifrado desplazamiento

--- src/shift/analysis.py
+++ src/shift/analysis.py
@@ -1,1 +1,22 @@
 # Module for crypto analysis
+
+
+from analysis import *
+
+def cifrado_desplazamiento(clave, mensaje_numeros):
+  lista_cifrada=[]
+
+  for i in range (len(mensaje_numeros)):
+    lista_cifrada.append((mensaje_numeros[i]+clave)%26)
+  return lista_cifrada
+
+
+
+
+def descifrado_desplazamiento(clave, mensaje_numeros):
+  lista_descifrada=[]
+
+  for i in range (len(mensaje_numeros)):
+    lista_descifrada.append((mensaje_numeros[i]-clave)%26)
+  return lista_descifrada
+
