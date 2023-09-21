77a41272c0d7209154db2602d69d1eb784e7f1aa
Alejandra Jaramillo <aljaramillom@unal.edu.co>
2023-09-21 16:03:01 -0500

    corregir 2 funciones de analisis(borrar 2 funciones)

--- src/shift/analysis.py
+++ src/shift/analysis.py
@@ -48,21 +48,3 @@ def descifrado_clave(clave, mensaje_numeros):
 
 
 
-
-def cifrado_desplazamiento(clave, mensaje_numeros):
-  lista_cifrada=[]
-
-  for i in range (len(mensaje_numeros)):
-    lista_cifrada.append((mensaje_numeros[i]+clave)%26)
-  return lista_cifrada
-
-
-
-
-def descifrado_desplazamiento(clave, mensaje_numeros):
-  lista_descifrada=[]
-
-  for i in range (len(mensaje_numeros)):
-    lista_descifrada.append((mensaje_numeros[i]-clave)%26)
-  return lista_descifrada
-
