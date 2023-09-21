c6d7993f5f26024e71dac7ef1d56db0229c5a32b
Alejandra Jaramillo <aljaramillom@unal.edu.co>
2023-09-21 15:59:04 -0500

    correccion funciones de analisis

--- src/shift/analysis.py
+++ src/shift/analysis.py
@@ -1,7 +1,53 @@
 # Module for crypto analysis
 
 
-from analysis import *
+list_str = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+
+def mayusculas(mensaje):
+  mensaje_mayuscula = mensaje.upper()
+  return mensaje_mayuscula
+
+
+
+def eliminar_espacios(mensaje):
+    mensaje_sin_espacios = mensaje.replace(" ", "")
+    return mensaje_sin_espacios
+
+
+def convertir_numeros(mensaje_sin_espacios):
+  list_number = []
+  for i in mensaje_sin_espacios:
+    list_number.append(list_str.index(i))
+  return list_number
+
+
+def convertir_letras(mensaje_desplazado):
+  string=""
+  for i in range(len(mensaje_desplazado)):
+    string= string + list_str[(mensaje_desplazado[i])]
+
+  return string
+
+
+def cifrado_clave(clave, mensaje_numeros):
+  lista_cifrada=[]
+
+  for i in range (len(mensaje_numeros)):
+    lista_cifrada.append((mensaje_numeros[i]+clave)%26)
+  return lista_cifrada
+
+
+
+def descifrado_clave(clave, mensaje_numeros):
+  lista_descifrada=[]
+
+  for i in range (len(mensaje_numeros)):
+    lista_descifrada.append((mensaje_numeros[i]-clave)%26)
+  return lista_descifrada
+
+
+
 
 def cifrado_desplazamiento(clave, mensaje_numeros):
   lista_cifrada=[]
--- src/shift/cipher.py
+++ src/shift/cipher.py
@@ -1,1 +1,27 @@
 # Module for cipher and decipher logic
+from analysis import *
+
+def cifrado_desplazamiento(clave, mensaje):
+    return convertir_letras(cifrado_clave(clave, convertir_numeros(eliminar_espacios(mensaje))))
+
+
+def descifrado_desplazamiento(clave, mensaje):
+    return convertir_letras(descifrado_clave(clave, convertir_numeros(eliminar_espacios(mensaje))))
+
+
+
+## prueba x
+
+mensaje = "attack at dawn"
+mensaje= mayusculas(mensaje)
+
+
+cifrado = cifrado_desplazamiento(17, mensaje)
+print(cifrado)
+
+
+
+
+mensaje_descifrado= descifrado_desplazamiento(17, 'RKKRTBRRKRURNE')
+
+print(mensaje_descifrado)
