7dc9eca0300d37389580584a1d44d93921af543c
Alejandra Jaramillo <aljaramillom@unal.edu.co>
2023-09-24 21:30:08 -0500

    cambiar funcion keygen

--- src/permutation/keygen.py
+++ src/permutation/keygen.py
@@ -1,8 +1,8 @@
 import random
 
-max_tamaño=8
-def generate_key(max_tamaño):
-    tamaño = random.randint(1, max_tamaño)
+
+def generate_key():
+    tamaño = random.randint(1, 8)
     numeros_disponibles = list(range(1, tamaño + 1))
     lista_aleatoria = random.sample(numeros_disponibles, tamaño)
     return lista_aleatoria
