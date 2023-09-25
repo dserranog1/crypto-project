d24f9ba61d4aa1eca9642e2f05cd834970ce2646
Alejandra Jaramillo <aljaramillom@unal.edu.co>
2023-09-24 21:33:52 -0500

    agregar funcion revisar clave y retornar clave en decifrar y cifrar

--- src/permutation/cipher.py
+++ src/permutation/cipher.py
@@ -6,16 +6,16 @@ def revisar_clave(clave):
     if not clave:
         clave = generate_key()
     elif not isinstance(clave, list):
-        clave = generate_key()
-       
+        clave = generate_key() 
     return clave
 
 
 
 def cifrado_permutacion(texto, clave_permutacion):
+  clave_permutacion = revisar_clave(clave_permutacion)
   return concatenar_listas_encriptadas(cifrado(crear_subpalabras(eliminar_especiales_y_numeros(eliminar_espacios(texto)),restar_uno_a_clave(clave_permutacion)),permutacion(restar_uno_a_clave(clave_permutacion)))),clave_permutacion
 
 
 def descifrado_permutacion(texto, clave_permutacion):
-
+  clave_permutacion = revisar_clave(clave_permutacion)
   return concatenar_listas_encriptadas(cifrado(crear_subpalabras(texto,clave_permutacion),permutacion_desencriptar(restar_uno_a_clave(clave_permutacion)))),clave_permutacion
