9ad2f5c02e976ba93e689ded78cb1b7c7a8b0c53
Hervin <daniel26rodriguez@gmail.com>
2023-09-25 22:06:18 -0500

    Update keygen.py

--- src/vigenere/keygen.py
+++ src/vigenere/keygen.py
@@ -1,1 +1,9 @@
 # Module for key generation
+
+from random import randint
+
+lista_claves = ['eagle', 'house', 'horse', 'heaven', 'rat', 'hell', 'esplendor', 'thisisnotakey', 'airplane', 'toronja']
+
+def generar_clave():
+  clave = lista_claves[randint(0,len(lista_claves))]
+  return clave
