ebc6ae91949d18eef6012c3211784a770f870e07
aacostas25 <aacostas@unal.edu.co>
2023-09-28 16:29:27 -0500

    Last update

--- src/affine/keygen.py
+++ src/affine/keygen.py
@@ -2,8 +2,6 @@
 import numpy as np
 
 def relativos(x,y):
-  print(x)
-  print(y)
   n= np.max([x,y])
   m = np.min([x,y])
   while(m>0):
@@ -28,5 +26,4 @@ def generate_key():
   num=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
   a = np.random.choice(A)
   b = np.random.choice(num)
-  print(a,b)
   return (a,b)
