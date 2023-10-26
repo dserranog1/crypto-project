77029581987eb19541165e1b44ebf7799906909c
Hervin <daniel26rodriguez@gmail.com>
2023-10-25 19:54:29 -0500

    Update keygen.py

--- src/block/des/keygen.py
+++ src/block/des/keygen.py
@@ -1,5 +1,9 @@
 # Module for key generation
 
 
-def generate_key():
-    return "des key!"
+import secrets
+
+def generar_key(tam_pt):
+    tam_key = tam_pt // 2
+    key = secrets.token_hex(tam_key)
+    return key.upper()
