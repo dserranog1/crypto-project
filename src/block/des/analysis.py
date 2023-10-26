2128bc14f6a716a6fb853a73b48f2e487d0b22b6
Hervin <daniel26rodriguez@gmail.com>
2023-10-25 19:53:35 -0500

    Update analysis.py

--- src/block/des/analysis.py
+++ src/block/des/analysis.py
@@ -1,1 +1,11 @@
 # Module for crypto analysis
+
+def hex2text(cadena_hexadecimal):
+  cadena_ascii = "".join([chr(int(cadena_hexadecimal[i : i + 2], 16)) for i in range(0, len(cadena_hexadecimal), 2)])
+  return cadena_ascii
+
+def text2hex(texto):
+  hexadecimal = ''.join([hex(ord(c))[2:].upper() for c in texto])
+  hexadecimal.upper()
+  return hexadecimal
+
