49b26a9874456e813e157e6c0abe954f63ac6c62
Hervin <daniel26rodriguez@gmail.com>
2023-10-27 19:21:22 -0500

    Update cipher.py

--- src/block/tdes/cipher.py
+++ src/block/tdes/cipher.py
@@ -8,53 +8,50 @@ def invalid_key(key):
     return True  # or False
 
 
-def t_des_encrypt(block_of_plain_text, key):
-    # implement t_des algorithm here
-    if not key or invalid_key(key):
-        # key gen and validation
-        key = generate_key()
+def t_des_encrypt(block_of_64_bits, key):
+    
+    # # implement t_des algorithm here
+    # if not key or invalid_key(key):
+    #     # key gen and validation
+    #     key = generate_key()
 
-    cipher_text_block = "tdes encrypt!"
-    return cipher_text_block, key
-
-
-def t_des_decrypt(block_of_cipher_text, key):
-    # implement t_des algorithm here
-    if not key or invalid_key(key):
-        # key gen and validation
-        key = generate_key()
-
-    plain_text_block = "tdes decrypt!"
-    return plain_text_block, key
-import hashlib
-
-def tres_claves(key):
-    key_1 = hex2bin(hashlib.sha256(key.encode()).hexdigest().upper()[:16])
-    key_2 = hex2bin(hashlib.sha256(key_1.encode()).hexdigest().upper()[:16])
-    key_3 = hex2bin(hashlib.sha256(key_2.encode()).hexdigest().upper()[:16])
-    return (key_1, key_2, key_3)
-
-def t_des_encrypt(block_of_64_bits, key, is_encrypting=True):
-
+    # cipher_text_block = "tdes encrypt!"
+    # return cipher_text_block, key
+    
     keys = key_1, key_2, key_3 = tres_claves(key)
-
-    if not is_encrypting:
-      keys = keys[::-1]
-
 
     # Cifrar el texto plano con el primer algoritmo DES
     text_cifrado_1,_ = des_encrypt(block_of_64_bits, key_1)
-    print('1era clave')
 
     # Cifrar el texto cifrado con el segundo algoritmo DES
     text_descifrado_2,_ = des_decrypt(text_cifrado_1, key_2)
-    print('2daa clave')
 
     # Cifrar el texto cifrado con el tercer algoritmo DES
     text_cifrado_2,_ = des_encrypt(text_descifrado_2, key_3)
-    print('3era clave')
 
-    return text_cifrado_2, key_3
+    return text_cifrado_2, keys
 
 def t_des_decrypt(block_of_64_bits, key):
-    return t_des_encrypt(block_of_64_bits, key, False)
+    
+    # # implement t_des algorithm here
+    # if not key or invalid_key(key):
+    #     # key gen and validation
+    #     key = generate_key()
+
+    # plain_text_block = "tdes decrypt!"
+    # return plain_text_block, key
+
+    key_3, key_2, key_1 = keys
+
+    # Cifrar el texto cifrado con el tercer algoritmo DES
+    text_cifrado_2,_ = des_decrypt(block_of_64_bits, key_1)
+
+    # Cifrar el texto cifrado con el segundo algoritmo DES
+    text_descifrado_2,_ = des_encrypt(text_cifrado_2, key_2)
+
+    # Cifrar el texto plano con el primer algoritmo DES
+    text_cifrado_1,_ = des_decrypt(text_descifrado_2, key_3)
+
+    return text_cifrado_1, keys
+    
+
