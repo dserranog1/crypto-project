2903e2c9d431806d6e2282eec643d2b3fe1b0f62
Hervin <daniel26rodriguez@gmail.com>
2023-10-27 19:17:21 -0500

    Update analysis.py

--- src/block/tdes/analysis.py
+++ src/block/tdes/analysis.py
@@ -1,1 +1,16 @@
 # Module for crypto analysis
+
+def tres_claves(key=None):
+  
+  if key == None or (len(key) != 64 and not is_binary(key)):
+    key = generate_key()
+
+    key_1 = hashlib.sha256(key.encode()).hexdigest().upper()[:16]
+    key_2 = hashlib.sha256(key_1.encode()).hexdigest().upper()[:16]
+    key_3 = hashlib.sha256(key_2.encode()).hexdigest().upper()[:16]
+    
+    # key_1 = hex2bin(hashlib.sha256(key.encode()).hexdigest().upper()[:16])
+    # key_2 = hex2bin(hashlib.sha256(key_1.encode()).hexdigest().upper()[:16])
+    # key_3 = hex2bin(hashlib.sha256(key_2.encode()).hexdigest().upper()[:16])
+    
+    return (key_1, key_2, key_3)
