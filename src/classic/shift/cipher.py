3a672ee3374328c8f6ab0c84d63b9aa93fc47cac
Alejandra Jaramillo <aljaramillom@unal.edu.co>
2023-11-11 12:29:12 -0500

    shift ASCCI

--- src/classic/shift/cipher.py
+++ src/classic/shift/cipher.py
@@ -13,59 +13,72 @@ def revisar_clave(clave):
     return clave
 
 
-def cifrado_desplazamiento(clave, mensaje):
-    # en lugar de modulo 26, que trabaje con mod 256(ascii)
-    clave = revisar_clave(clave)
-    return (
-        convertir_letras(
-            cifrado_clave(
-                clave,
-                convertir_numeros(
-                    eliminar_espacios(
-                        eliminar_especiales_y_numeros(mayusculas(mensaje))
-                    )
-                ),
-            )
-        ),
-        clave,
-    )
+#def cifrado_desplazamiento(clave, mensaje):
+#    # en lugar de modulo 26, que trabaje con mod 256(ascii)
+#    clave = revisar_clave(clave)
+#    return (
+#        convertir_letras(
+#            cifrado_clave(
+#                clave,
+#                convertir_numeros(
+#                    eliminar_espacios(
+#                        eliminar_especiales_y_numeros(mayusculas(mensaje))
+#                    )
+#                ),
+#            )
+#        ),
+#        clave,
+#    )
+#
+#
+#def descifrado_desplazamiento(clave, mensaje):
+#    clave = revisar_clave(clave)
+#    return (
+#        convertir_letras(
+#            descifrado_clave(
+#                clave,
+#                convertir_numeros(
+#                    eliminar_espacios(
+#                        eliminar_especiales_y_numeros(mayusculas(mensaje))
+#                    )
+#                ),
+#            )
+#        ),
+#        clave,
+#    )
+#
+
+#def attack(texto_cifrado):
+#    lista_attack = []
+#    for i in range(25):
+#        des = descifrado_desplazamiento(str(i), texto_cifrado)
+#
+#        lista_attack.append({"clave": str(i), "texto_descifrado": des[0]})
+#    return lista_attack
+#
 
 
-def descifrado_desplazamiento(clave, mensaje):
+def cifrado_desplazamiento(texto, clave):
     clave = revisar_clave(clave)
-    return (
-        convertir_letras(
-            descifrado_clave(
-                clave,
-                convertir_numeros(
-                    eliminar_espacios(
-                        eliminar_especiales_y_numeros(mayusculas(mensaje))
-                    )
-                ),
-            )
-        ),
-        clave,
-    )
+    resultado = ""
+
+    for char in texto:
+        if char.isprintable():  # Cifra caracteres imprimibles y deja otros sin cambios
+            start = 32  # El valor ASCII del primer caracter imprimible
+            end = 255  # El valor ASCII del último caracter imprimible
+            resultado += chr((ord(char) - start + clave) % (end - start + 1) + start)
+        else:
+            resultado += char
+
+    return resultado
+
+
+def descifrado_desplazamiento(texto_cifrado, clave):
+    clave = revisar_clave(clave)
+    return cifrado_desplazamiento(texto_cifrado, -clave)
 
 
 def attack(texto_cifrado):
-    lista_attack = []
-    for i in range(25):
-        des = descifrado_desplazamiento(str(i), texto_cifrado)
-
-        lista_attack.append({"clave": str(i), "texto_descifrado": des[0]})
-    return lista_attack
-
-
-# prueba x
-
-# mensaje = "attack at dawn"
-#
-# mensaje = mayusculas(mensaje)
-# cifrado = cifrado_desplazamiento(100, mensaje)
-# print(f'cifrado: {cifrado}')
-# mensaje_descifrado = descifrado_desplazamiento(100, 'WPPWYGWPZWSJ')
-# print(f'descifrado: {mensaje_descifrado}')
-#
-# texto_cifrado= 'wppwygwpzws'
-# print(attack(texto_cifrado))
+    for desplazamiento in range(1, 257):  # Rango completo de ASCII extendido
+        texto_descifrado = descifrado_desplazamiento(texto_cifrado, desplazamiento)
+        print(f"Intento con desplazamiento {desplazamiento}: {texto_descifrado}")
