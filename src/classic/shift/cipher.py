5dbb4b72b28dfdd9d40d6a1837dd25705404dd3c
Alejandra Jaramillo <aljaramillom@unal.edu.co>
2023-11-11 14:53:10 -0500

    shift ASCCI correccion 3

--- src/classic/shift/cipher.py
+++ src/classic/shift/cipher.py
@@ -1,20 +1,19 @@
-## Module for cipher and decipher logic
+### Module for cipher and decipher logic
 from .analysis import *
 from .keygen import *
-
-
-def revisar_clave(clave):
-    if not clave:
-        clave = generate_key()
-    elif not clave.isdigit():
-        clave = generate_key()
-    else:
-        clave = int(clave)
-    return clave
-
-
+#
+#
+#def revisar_clave(clave):
+#    if not clave:
+#        clave = generate_key()
+#    elif not clave.isdigit():
+#        clave = generate_key()
+#    else:
+#        clave = int(clave)
+#    return clave
+#
+#
 #def cifrado_desplazamiento(clave, mensaje):
-#    # en lugar de modulo 26, que trabaje con mod 256(ascii)
 #    clave = revisar_clave(clave)
 #    return (
 #        convertir_letras(
@@ -47,7 +46,7 @@ def revisar_clave(clave):
 #        clave,
 #    )
 #
-
+#
 #def attack(texto_cifrado):
 #    lista_attack = []
 #    for i in range(25):
@@ -56,25 +55,44 @@ def revisar_clave(clave):
 #        lista_attack.append({"clave": str(i), "texto_descifrado": des[0]})
 #    return lista_attack
 #
+#
+#
+#
+#
 
+import random
 
-def cifrado_desplazamiento(clave, texto):
-    clave = revisar_clave(clave)
+def revisar_clave(clave):
+    if not clave:
+        clave = generate_key()
+    elif not clave.isdigit():
+        clave = generate_key()
+    else:
+        clave = int(clave)
+    return clave
+
+def generate_key():
+    return random.randint(1, 26)
+
+def cifrado_desplazamiento(desplazamiento_str, texto):
+    desplazamiento =  revisar_clave(desplazamiento_str)
     resultado = ""
-
     for char in texto:
-        resultado += chr((ord(char) + clave) % 256)
+        resultado += chr((ord(char) + desplazamiento) % 256)
 
-    return resultado
+    return (resultado, desplazamiento)
 
+def descifrado_desplazamiento(desplazamiento_str, texto_cifrado):
+    desplazamiento =  revisar_clave(desplazamiento_str)
+    resultado = ""
+    for char in texto_cifrado:
 
+            resultado += chr((ord(char) - desplazamiento) % 256)
 
-def descifrado_desplazamiento(clave, texto_cifrado):
-    clave = revisar_clave(clave)
-    return cifrado_desplazamiento(texto_cifrado, -clave)
+    return (resultado, desplazamiento)
 
 
 def attack(texto_cifrado):
     for desplazamiento in range(1, 257):  # Rango completo de ASCII extendido
-        texto_descifrado = descifrado_desplazamiento(texto_cifrado, desplazamiento)
-        print(f"Intento con desplazamiento {desplazamiento}: {texto_descifrado}")
+        texto_descifrado, clave_descifrado = descifrado_desplazamiento(str(desplazamiento), texto_cifrado)
+        print(f"Intento con desplazamiento {clave_descifrado}: {texto_descifrado}")
