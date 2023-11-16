e55427e9e1f09e4d517f80592ddb0e470dcc950c
Hervin <daniel26rodriguez@gmail.com>
2023-11-15 22:15:50 -0500

    Update keygen.py

--- src/classic/vigenere/keygen.py
+++ src/classic/vigenere/keygen.py
@@ -1,22 +1,19 @@
 # Module for key generation
 
-from random import randint
-
-lista_claves = [
-    "EAGLE",
-    "HOUSE",
-    "HORSE",
-    "HEAVEN",
-    "RAT",
-    "HELL",
-    "ESPLENDOR",
-    "THISISNOTAKEY",
-    "AIRPLANE",
-    "TORONJA",
-]
+import string 
+import random
+import hashlib
 
 
 # generar claves completamente aleatorias
 def generar_clave():
-    clave = lista_claves[randint(0, len(lista_claves))]
-    return clave
+    
+    # Genera un tamaño aleatorio para el texto entre 5 y 20 caracteres
+    tamano = randint(5, 20)
+
+    # Genera caracteres aleatorios para el texto utilizando el rango de caracteres imprimibles ASCII
+    clave_aleatoria = ''.join(random.choice(string.ascii_lowercase) for _ in range(tamano))
+
+    #hash_clave = hashlib.sha256(clave_aleatoria.encode()).hexdigest()
+
+    return clave_aleatoria #hash_clave
