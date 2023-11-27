c3d802408e784a41f425b38af06d713a2e6079c0
aacostas25 <aacostas@unal.edu.co>
2023-11-26 20:22:09 -0500

    Rabin, fixed

--- src/public_key/rabin/cipher.py
+++ src/public_key/rabin/cipher.py
@@ -25,22 +25,31 @@ def gcd(a, b):
 
     return old_r, old_s, old_t
 
+def texto_a_silabas(texto,a):
+    # Eliminar espacios y convertir a mayúsculas
+    texto_sin_espacios = texto.replace(" ", "").lower()
 
-def invalid_key(key):
-    # implement key validation logic
-    return False
+    # Crear una lista de sílabas de tres letras
+    lista_silabas = [texto_sin_espacios[i:i+a] for i in range(0, len(texto_sin_espacios), a)]
 
-
-def rabin_encrypt(clear_text: str, key: str):
-    n = int(key)
-    m = int.from_bytes(clear_text.encode("utf-8"), byteorder="big")
-    encrypted_text = cifrar(m, n)
-    return encrypted_text, key
+    return lista_silabas
 
 
-def rabin_decrypt(encrypted_text: str, key: str):
-    key = key.split(" ")
-    key = (int(key[0]), int(key[1]))
+def decode_integers_to_bytes(integers):
+    byte_list = []
+    for num in integers:
+        byte_representation = num.to_bytes((num.bit_length() + 7) // 8, byteorder='big')
+        byte_list.append(byte_representation)
+    return byte_list
+
+def bytes_list_to_strings(byte_list):
+    string_list = []
+    for byte_repr in byte_list:
+        decoded_string = byte_repr.decode('utf-8', 'replace')
+        string_list.append(decoded_string)
+    return string_list
+
+def desencriptar(encrypted_text, key):
     p, q = key
     n = p * q
     mp = pow(int(encrypted_text), (p + 1) // 4, p)
@@ -51,13 +60,40 @@ def rabin_decrypt(encrypted_text: str, key: str):
     r2 = n - r1
     r3 = (yp * p * mq - yq * q * mp) % n
     r4 = n - r3
-    display_message = ""
-    for b in [r1, r2, r3, r4]:
-        # Decode bytes to string using UTF-8
-        display_message += (
-            b.to_bytes((b.bit_length() + 7) // 8, byteorder="big").decode(
-                "utf-8", "replace"
-            )
-            + "\n"
-        )
+    return r1,r2,r3,r4
+
+def invalid_key(key):
+    # implement key validation logic
+    return False
+
+
+def rabin_encrypt(clear_text: str, key: str):
+    n = int(key)
+    clear_text = texto_a_silabas(clear_text,5)
+    cifrado = []
+    for i in clear_text:
+      m = int.from_bytes(i.encode('utf-8'), byteorder='big')
+      c = cifrar(m, n)
+      cifrado.append(str(c))
+    encrypted_text = ''.join(cifrado)
+    return encrypted_text, key
+
+def rabin_decrypt(encrypted_text: str, key: str):
+    key = key.split(" ")
+    key = (int(key[0]), int(key[1]))
+    p, q = key
+    encrypted_text = texto_a_silabas(encrypted_text,12)
+    print('encrypted_text',encrypted_text)
+    descifrado = []
+    for j in encrypted_text:
+      d = desencriptar(j, (p, q))
+      print(j,d)
+      descifrado.append(d)
+    final = []
+    for k in descifrado:
+      byte_list = decode_integers_to_bytes(k)
+      decoded_strings = bytes_list_to_strings(byte_list)
+      final.append([item for item in decoded_strings if item.isalnum()] )
+    display_message = [''.join(sublist) for sublist in final]
+    display_message = ''.join(display_message)
     return display_message, key
